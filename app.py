a1c3909887e8a8e02e7676654dec6fb2bcd51957
Itzel-Rodriguez <54225576+Itzel-Rodriguez@users.noreply.github.com>
2024-04-12 12:21:30 -0600

    Cambio de biblioteca e Implementación de SocketIO
    Cambié la biblioteca Crypto por Cryptodome y aparentemente el funcionamiento es el mismo. 
    
    Esta versión tiene la implementación de SocketIO a la generación del par de llaves publica y privada, a la generación del secreto su cifrado. 
    Dado que se mantuvo la implementación de SocketIO y puede llegar a dar problemas en entornos como Visual Studio Code, se recomienda que el proyecto se corra desde terminal

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
-#SocketIO para la comunicación bidireccional con WebSockets
 from flask_socketio import SocketIO, join_room, leave_room, send
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import PKCS1_OAEP
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from os import urandom
+from base64 import urlsafe_b64encode, urlsafe_b64decode
+import websockets
 
-#import ssl #NEW
-
-app = Flask(__name__)
+app = Flask(_name_)
 app.config['SECRET_KEY'] = 'secret_key'
-#Instancia del SocketIO
 socketio = SocketIO(app)
 
-
-# Configuración SSL
-#certh_path = 'cert.pem' #NEW
-#key_path = 'key.pem' #NEW
-#context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER) #NEW
-#context.load_cert_chain(certh_path, key_path) #OJO, FALTA CONSEGUIRLOS #NEW
-#socketio = SocketIO(app, cors_allowed_origins='*', async_mode='threading', ssl_context=context) #NEW. OJO HAY QUE REEMPLAZAR POR LÍNEA 10
-
-#Diccionario para almacenar los usuarios que se van registrando
-users = {}  
+# Diccionario para almacenar los usuarios que se van registrando
+users = {}
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST": #Si POST toma el nombre del usuario y mándalo a la página del chat
         username = request.form['username']
+        publica = generate_keys_RSA(username) ################################### AQUI!!!!! AQUI MERO SE GENERAN LAS LLAVES PUB Y PRIV (se guardan)
+        password = request.form['password']
+        secreto = generate_key_PBKDF(str(password),urandom(16))  ############AQUI!!!!! AQUI MERO SE GENERA EL SECRETO (LA SIMETRICA)
+
+        cipher = PKCS1_OAEP.new(publica) #################### AQUI!!!! AQUI MERO SE CIFRA LA SIMETRICA CON LA ASIMETRICA
+        secreto = cipher.encrypt("hola".encode())
+        print(secreto)
+        #binascii.hexlify(encrypted_message).decode()
         return redirect(url_for('chat', name=username))
     return render_template('login.html') #Si GET mándale la plantilla login
 
@@ -33,13 +39,10 @@ def chat(name):
     return render_template('chat.html', name=name)
 
 @socketio.on('join')
-#data es un diccionario para encontrar el username y room que se envía del cliente al servidor
 def on_join(data):
-    username = data['username'] 
+    username = data['username']
     room = data['room']
-    #Se une a una sala para que pueda interactuar con los otros usuarios
     join_room(room)
-    #Se registra que cierto usuario está en cierto room
     users[username] = room
     send(f"{username} se unió al chat", to=room)
 
@@ -58,6 +61,33 @@ def on_message(data):
     message = data['message']
     send(f"{username}: {message}", to=room)
 
-if __name__ == '__main__':
+def generate_keys_RSA(username):
+    key = RSA.generate(2048)  # Genera una clave RSA de 2048 bits
+    private_key = key.export_key()
+    public_key = key.publickey().export_key()
+    with open("private_"+username+".pem", "wb") as pr_file:
+        pr_file.write(private_key)
+
+    # Guardar la clave pública
+    with open("public_"+username+".pem", "wb") as pu_file:
+        pu_file.write(public_key)
+    return key.publickey()
+
+def generate_key_PBKDF(password: str, salt: bytes, iterations: int = 100000, key_length: int = 32) -> bytes:
+    """Genera una llave a partir de una contraseña usando PBKDF2."""
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=key_length,
+        salt=salt,
+        iterations=iterations,
+        backend=default_backend()
+    )
+    return kdf.derive(password.encode())
+
+'''def encrypt_with_public_key(message: bytes, public_key: bytes) -> bytes:
+    public_key = RSA.importKey(public_key)
+    cipher = PKCS1_OAEP.new(public_key)
+    return cipher.encrypt(message)'''
+
+if _name_ == '_main_':
     socketio.run(app, host='localhost', port=81, debug=True)
-    #socketio.run(app, host='localhost', port=81, debug=True, ssl_context=context) #NEW
