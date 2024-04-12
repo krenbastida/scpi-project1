4f8e260952bd766aab57301b25bd6d9dc46d9cfe
Zuriel Zárate García <54085182+zagazu73@users.noreply.github.com>
2024-04-11 20:33:29 -0600

    Generación y cifrado
    Ya se generan par de llaves publica y privada, se genera el secreto y se cifra, a su vez, con la llave pública. Falta poder enviar mensajes.
    
    -Se agregan archivos con las funciones pertinentes para firma digital y validación de la firma, cifrado de mensaje e integridad con funciones hash

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
-#SocketIO para la comunicación bidireccional con WebSockets
-from flask_socketio import SocketIO, join_room, leave_room, send
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from os import urandom
+from base64 import urlsafe_b64encode, urlsafe_b64decode
+import websockets
 
-import ssl #NEW
+#Python -m flask run (para ejecutar proyecto desde consola)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key'
-#Instancia del SocketIO
-#socketio = SocketIO(app)
-
-# Configuración SSL
-certh_path = 'cert.pem'
-key_path = 'key.pem'
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER) #NEW
-context.load_cert_chain(certh_path, key_path) #OJO, FALTA CONSEGUIRLOS #NEW
-socketio = SocketIO(app, cors_allowed_origins='*', async_mode='threading', ssl_context=context) #NEW. OJO HAY QUE REEMPLAZAR POR LÍNEA 10
-
-#Diccionario para almacenar los usuarios que se van registrando
-users = {}  
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST": #Si POST toma el nombre del usuario y mándalo a la página del chat
         username = request.form['username']
+        publica = generate_keys_RSA(username) ################################### AQUI!!!!! AQUI MERO SE GENERAN LAS LLAVES PUB Y PRIV (se guardan)
+        password = request.form['password']
+        secreto = generate_key_PBKDF(str(password),urandom(16))  ############AQUI!!!!! AQUI MERO SE GENERA EL SECRETO (LA SIMETRICA)
+
+        cipher = PKCS1_OAEP.new(publica) #################### AQUI!!!! AQUI MERO SE CIFRA LA SIMETRICA CON LA ASIMETRICA
+        secreto = cipher.encrypt("hola".encode())
+        print(secreto)
+        #binascii.hexlify(encrypted_message).decode()
         return redirect(url_for('chat', name=username))
     return render_template('login.html') #Si GET mándale la plantilla login
 
@@ -31,32 +34,38 @@ def login():
 def chat(name):
     return render_template('chat.html', name=name)
 
-@socketio.on('join')
-#data es un diccionario para encontrar el username y room que se envía del cliente al servidor
-def on_join(data):
-    username = data['username'] 
-    room = data['room']
-    #Se une a una sala para que pueda interactuar con los otros usuarios
-    join_room(room)
-    #Se registra que cierto usuario está en cierto room
-    users[username] = room
-    send(f"{username} se unió al chat", to=room)
+def generate_keys_RSA(username):
+    key = RSA.generate(2048)  # Genera una clave RSA de 2048 bits
+    private_key = key.export_key()
+    public_key = key.publickey().export_key()
+    with open("private_"+username+".pem", "wb") as pr_file:
+        pr_file.write(private_key)
 
-@socketio.on('leave')
-def on_leave(data):
-    username = data['username']
-    room = users[username]
-    leave_room(room)
-    del users[username]
-    send(f"{username} ha salido del chat", to=room)
+    # Guardar la clave pública
+    with open("public_"+username+".pem", "wb") as pu_file:
+        pu_file.write(public_key)
+    return key.publickey()
 
-@socketio.on('message')
-def on_message(data):
-    username = data['username']
-    room = users[username]
-    message = data['message']
-    send(f"{username}: {message}", to=room)
+def generate_key_PBKDF(password: str, salt: bytes, iterations: int = 100000, key_length: int = 32) -> bytes:
+    """Genera una llave a partir de una contraseña usando PBKDF2."""
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=key_length,
+        salt=salt,
+        iterations=iterations,
+        backend=default_backend()
+    )
+    return kdf.derive(password.encode())
 
 if __name__ == '__main__':
-    #socketio.run(app, host='localhost', port=81, debug=True)
-    socketio.run(app, host='localhost', port=81, debug=True, ssl_context=context)
+    app.run(host='localhost', port=81, debug=True)
+
+'''
+# Guardar la clave privada
+    with open("private.pem", "wb") as pr_file:
+        pr_file.write(private_key)
+
+    # Guardar la clave pública
+    with open("public.pem", "wb") as pu_file:
+        pu_file.write(public_key)
+'''
